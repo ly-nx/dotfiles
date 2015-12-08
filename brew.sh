753cc14fc66e6794c3a1fa6743fd54f3a07f63fe
lynx <a.kharus@gmail.com>
2015-12-08 17:27:36 +0300

    tmux updated

--- brew.sh
+++ brew.sh
@@ -100,6 +100,9 @@ brew install tree
 brew install webkit2png
 brew install zopfli
 brew install reattach-to-user-namespace
+brew install macvim
+brew install tmux
+brew cask install iterm2
 
 # Remove outdated versions from the cellar.
 brew cleanup
