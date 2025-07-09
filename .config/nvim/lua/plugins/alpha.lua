f372ca259360e4e059beee49f74da2a1e547371e
lynx <a.kharus@gmail.com>
2025-07-10 01:55:35 +0300

    minor

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -8,7 +8,6 @@ return {
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
-
     dashboard.section.header.val = {
       [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀              ⠀⠀⠀⠀⠀⢀⡆⠀⠀⠀⠀⠀⠀⠀⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀      ⠀]],
       [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀              ⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀      ⠀]],
@@ -37,33 +36,27 @@ return {
       [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀           ⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
       [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀              ⠀⠀⠈⠻⠿⠿⠿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
       [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⠀⠀           ⠀⠈⠻⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-      [[⠀⠀   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀           ⠀⠀⠀⠘⠏⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
-    }
-
-    _Gopts = {
-      position = "center",
-      hl = "Type",
-      -- wrap = "overflow";
+      [[⠀⠀   ⠀⠀⠀⠀ ⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀           ⠀⠀⠀⠘⠏⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
     }
 
     -- Set menu
     dashboard.section.buttons.val = {
       -- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
-      dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+      -- dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("f", "   Find config file", ":cd $HOME/.config/nvim | Telescope find_files<CR>"),
       dashboard.button("p", "   Find project file", ":cd $HOME/Projects | Telescope find_files<CR>"),
-      dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
-      dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+      -- dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
+      dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>", {}),
       dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
       dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
       dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
       dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
     }
+    dashboard.section.buttons.opts.spacing = 0
 
     dashboard.opts.opts.noautocmd = true
-    alpha.setup(dashboard.opts)
 
-    require("alpha").setup(dashboard.opts)
+    alpha.setup(dashboard.opts)
 
     vim.api.nvim_create_autocmd("User", {
       pattern = "LazyVimStarted",
@@ -72,8 +65,8 @@ return {
         local count = (math.floor(stats.startuptime * 100) / 100)
         dashboard.section.footer.val = {
           "󱐌 " .. stats.count .. " plugins loaded in " .. count .. " ms",
-          " ",
-          "      welp..",
+          -- " ",
+          -- "      welp..",
         }
         pcall(vim.cmd.AlphaRedraw)
       end,
