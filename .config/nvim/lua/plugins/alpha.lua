460b56ffc7865e4f44e6b1f2a36797bbf98f9c9a
lynx <icy.lynx@gmail.com>
2025-08-02 21:59:29 +0300

    nvim alpha dirs

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -43,11 +43,11 @@ return {
     dashboard.section.buttons.val = {
       -- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
       -- dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "   Find config file", ":cd $HOME/.config/nvim | Telescope find_files<CR>"),
+      dashboard.button("f", "   Find .config file", ":cd $HOME/.config | Telescope find_files<CR>"),
       dashboard.button("p", "   Find project file", ":cd $HOME/Projects | Telescope find_files<CR>"),
       -- dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
       dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>", {}),
-      dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("c", "   Config", "cd $HOME/.config/nvim/lua/plugins | Telescope find_files<CR>"),
       dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
       dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
       dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
